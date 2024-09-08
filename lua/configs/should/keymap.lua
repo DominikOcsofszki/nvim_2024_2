95c35cd686215f657a07cd94c283d6acc9b9c4cb
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2024-09-08 21:53:21 +0200

    add keybind: jk and kj for esc

--- lua/configs/should/keymap.lua
+++ lua/configs/should/keymap.lua
@@ -2,4 +2,6 @@
 vim.keymap.set('n','<leader><leader>',':NvimTreeToggle<cr>',{})
 vim.keymap.set('n','\\',':w<cr>',{})
 vim.keymap.set('n','|',':q<cr>',{})
+vim.keymap.set('n','jk','<esc>',{})
+vim.keymap.set('n','kj','<esc>',{})
 
